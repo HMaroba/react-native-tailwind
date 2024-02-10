636195275375e28f6c421702ed80558d93d95e16
Coffee58511867 <marobahlalele910@gmail.com>
2024-02-11 01:05:21 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View , Button, TextInput, TouchableOpacity} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl">Open up App.js to start working on your app!</Text>
+      <View className="w-full p-10">
+      <TextInput placeholder="Username" className="w-full p-2 rounded-md border-2 border-blue-400"/>
+      <TouchableOpacity className="w-full text-center bg-blue-700 p-3 mt-4 text-white rounded-2xl">
+        <Text className="text-white text-lg text-center">Sign in</Text>
+      </TouchableOpacity>
+      </View> 
+   
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
